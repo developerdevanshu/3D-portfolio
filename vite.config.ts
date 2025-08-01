b1e7973a0333d0240ed60b9ef5e18a217e430e68
developerdevanshu <devanshuagarwal164@gmail.com>
2025-08-01 17:06:58 +0530

    vite configuration updating

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio-Website/',
+  base: '/', // ✅ required for Netlify
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    assetsDir: 'assets',
+  },
 })
